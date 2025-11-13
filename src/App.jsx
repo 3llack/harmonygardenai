73454ed6c1f7b47f261be91a296b0984165ed16f
3llack <ayomiidx@gmail.com>
2025-11-13 16:14:58 +0100

    ...

--- src/App.jsx
+++ src/App.jsx
@@ -116,7 +116,6 @@ function App() {
             <div className="text-center">
               <div 
                 className="d-inline-flex justify-content-center flex-wrap gap-4 mt-4 logos"
-                style={{ maxHeight: '114px', overflow: 'hidden' }}
               >
                 {[
                   { src: Lekki, label: 'Lekki Aviation', size: 45 },
